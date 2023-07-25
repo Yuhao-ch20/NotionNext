65da567b2a856a5822351edb8a86e91a69571605
Yuhao-ch20 <138643058+Yuhao-ch20@users.noreply.github.com>
2023-07-25 15:31:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -227,14 +227,13 @@ const BLOG = {
   COMMENT_GISCUS_CROSSORIGIN:
       process.env.NEXT_PUBLIC_COMMENT_GISCUS_CROSSORIGIN || 'anonymous', // 你的 Giscus 可以跨網域, 預設 'anonymous'
 
-  COMMENT_CUSDIS_APP_ID: process.env.NEXT_PUBLIC_COMMENT_CUSDIS_APP_ID || '0e742c34-d313-46f1-b5ee-d7deec73be34', // data-app-id 36位 see https://cusdis.com/
+  COMMENT_CUSDIS_APP_ID: process.env.NEXT_PUBLIC_COMMENT_CUSDIS_APP_ID || '', // data-app-id 36位 see https://cusdis.com/
   COMMENT_CUSDIS_HOST:
       process.env.NEXT_PUBLIC_COMMENT_CUSDIS_HOST || 'https://cusdis.com', // data-host, change this if you're using self-hosted version
   COMMENT_CUSDIS_SCRIPT_SRC:
       process.env.NEXT_PUBLIC_COMMENT_CUSDIS_SCRIPT_SRC ||
       'https://cusdis.com/js/cusdis.es.js', // change this if you're using self-hosted version
-  COMMENT_CUSDIS_LANG: process.env.NEXT_PUBLIC_COMMENT_CUSDIS_LANG || 'en',
-
+  
   // gitalk评论插件 更多参考 https://gitalk.github.io/
   COMMENT_GITALK_REPO: process.env.NEXT_PUBLIC_COMMENT_GITALK_REPO || '', // 你的Github仓库名，例如 'NotionNext'
   COMMENT_GITALK_OWNER: process.env.NEXT_PUBLIC_COMMENT_GITALK_OWNER || '', // 你的用户名 e.g tangly1024
